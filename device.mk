c7b38d304f1048a4b9c915f979f7deeda8bbac46
lilindian14 <jaimesequeira14@gmail.com>
2018-11-24 08:18:22 +1300

    added device.mk and gta2swifi.mk

---
+++ device.mk
@@ -0,0 +1,13 @@
+#Inherit from vendor
+$(call inherit-product-if-exists, vendor/samsung/gta2swifi/gta2swifi-vendor.mk)
+
+# Inherit from common
+$(call inherit-product, device/samsung/gta2swifi/gta2swifi-common.mk)
+
+LOCAL_PATH := device/samsung/gta2swifi
+
+# System properties
+-include $(LOCAL_PATH)/system_prop.mk
+
+# Common overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
--- gta2swifi.mk
+++ gta2swifi.mk
@@ -1,16 +1,15 @@
 
 # Inherit from common
-$(call inherit-product, device/samsung/gta2swifi/lineage.mk)
+$(call inherit-product, device/samsung/gta2swifi/tabA.mk)
 
 $(call inherit-product, device/samsung/gta2swifi/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := gta2swifi
-PRODUCT_NAME := lineage_gta2swifi
+PRODUCT_NAME := gta2swifi
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-T380
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_CHARACTERISTICS := tablet
 
-# TODO: what is this??
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
